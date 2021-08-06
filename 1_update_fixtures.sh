5b6f51007605d4e434ef11a1e59c5c79df05c251
Jace Manshadi <jmanshad@protonmail.com>
2021-08-05 22:06:51 -0700

    updating folders
    Signed-off-by: Jace Manshadi <jmanshad@protonmail.com>

--- 1_update_fixtures.sh
+++ 1_update_fixtures.sh
@@ -2,18 +2,17 @@
 
 set -e -o xtrace
 
-rm ../../migrations/fixtures/* || true
 
-scp "../../migrations/0_create_fixture_jsons.sh" csss@sfucsss.org:"/home/csss/create_jsons.sh"
+scp "0_create_fixture_jsons.sh" csss@sfucsss.org:"/home/csss/create_jsons.sh"
 ssh csss@sfucsss.org "/home/csss/create_jsons.sh"
 scp -r csss@sfucsss.org:"/home/csss/csss-site/*.json" .
 ssh csss@sfucsss.org "rm -fr /home/csss/csss-site/*.json"
 ssh csss@sfucsss.org "rm /home/csss/create_jsons.sh"
 
-mv about.json ../../migrations/fixtures/.
-python3 ../../migrations/update_confidential_info.py
-mv auth.json ../../migrations/fixtures/.
-mv announcements.json ../../migrations/fixtures/.
-mv django_mailbox.json ../../migrations/fixtures/.
-mv elections.json ../../migrations/fixtures/.
-mv resource_management.json ../../migrations/fixtures/.
+mv about.json ~/fixtures/.
+python3 ./update_confidential_info.py
+mv auth.json ~/fixtures/.
+mv announcements.json ~/fixtures/.
+mv django_mailbox.json ~/fixtures/.
+mv elections.json ~/fixtures/.
+mv resource_management.json ~/fixtures/.
