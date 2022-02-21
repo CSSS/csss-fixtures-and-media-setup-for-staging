74d66524a421f9a746ae9b7860058ae80e84a622
Jace Manshadi <jmanshad@protonmail.com>
2022-02-21 13:36:47 -0800

    factoring fixture creator for wall_e into the main script

--- download_fixtures_and_media.sh
+++ download_fixtures_and_media.sh
@@ -5,7 +5,8 @@ set -e -o xtrace
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${BASE_DIR}"
 
-scp "csss_site_create_fixture_jsons.sh" csss@sfucsss.org:"/home/csss/create_jsons.sh"
+## Website media
+scp "create_fixture_jsons_csss_site.sh" csss@sfucsss.org:"/home/csss/create_jsons.sh"
 ssh csss@sfucsss.org "/home/csss/create_jsons.sh"
 scp -r csss@sfucsss.org:"/home/csss/csss-site/*.json" .
 ssh csss@sfucsss.org "rm -fr /home/csss/csss-site/*.json"
@@ -29,10 +30,12 @@ scp -r csss@sfucsss.org:"/home/csss/static_root/about_static/exec-photos" /mnt/d
 rm -fr /mnt/dev_csss_website_media/exec-photos/.git
 
 
-scp "wall_e_create_fixtures_json.sh" csss@jenkins.sfucsss.org:"/home/csss/create_jsons.sh"
+## Wall_e media
+
+scp "create_fixture_jsons_wall_e.sh" csss@jenkins.sfucsss.org:"/home/csss/create_jsons.sh"
 ssh csss@jenkins.sfucsss.org "/home/csss/create_jsons.sh"
 scp -r csss@jenkins.sfucsss.org:"/home/csss/wall_e.json" .
 ssh csss@jenkins.sfucsss.org "rm -fr /home/csss/wall_e.json"
 ssh csss@jenkins.sfucsss.org "rm /home/csss/create_jsons.sh"
-mkdir /mnt/dev_csss_wall_e/fixtures || true
+mkdir -p /mnt/dev_csss_wall_e/fixtures || true
 mv wall_e.json /mnt/dev_csss_wall_e/fixtures/.
