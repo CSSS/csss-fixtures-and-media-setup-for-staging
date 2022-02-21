6cba160397fcf2dc67c345e738f7a9af741677f7
Jace Manshadi <jmanshad@protonmail.com>
2022-02-21 13:26:03 -0800

    ensuring file is moved from container to localhost

--- wall_e_create_fixtures_json.sh
+++ wall_e_create_fixtures_json.sh
@@ -4,4 +4,4 @@ set -e -o xtrace
 
 docker exec PRODUCTION_MASTER_wall_e python3 django_db_orm_manage.py dumpdata --indent 4 --output wall_e.json
 docker cp PRODUCTION_MASTER_wall_e:/usr/src/app/wall_e.json .
-docker rm PRODUCTION_MASTER_wall_e:/usr/src/app/wall_e.json
+docker exec PRODUCTION_MASTER_wall_e rm wall_e.json
