b7595d4a88d46244497d95f865444c1feea41d91
Jace Manshadi <jaymanshad@proton.me>
2023-12-01 13:06:08 -0800

    creating a JSON for each model

--- create_fixture_jsons_wall_e.sh
+++ create_fixture_jsons_wall_e.sh
@@ -2,6 +2,18 @@
 
 set -e -o xtrace
 
-docker exec PRODUCTION_MASTER_wall_e python3 django_manage.py dumpdata --indent 4 --output wall_e.json
-docker cp PRODUCTION_MASTER_wall_e:/usr/src/app/wall_e.json .
-docker exec PRODUCTION_MASTER_wall_e rm wall_e.json
+docker exec PRODUCTION_MASTER_wall_e python3 django_manage.py dumpdata wall_e_models.banrecord --indent 4 --output banrecords.json
+docker exec PRODUCTION_MASTER_wall_e python3 django_manage.py dumpdata wall_e_models.commandstat --indent 4 --output commandstats.json
+docker exec PRODUCTION_MASTER_wall_e python3 django_manage.py dumpdata wall_e_models.profilebucketinprogress --indent 4 --output profilebucketsinprogress.json
+docker exec PRODUCTION_MASTER_wall_e python3 django_manage.py dumpdata wall_e_models.userpoint --indent 4 --output userpoints.json
+docker exec PRODUCTION_MASTER_wall_e python3 django_manage.py dumpdata wall_e_models.level --indent 4 --output levels.json
+docker exec PRODUCTION_MASTER_wall_e python3 django_manage.py dumpdata wall_e_models.reminder --indent 4 --output reminders.json
+
+docker cp PRODUCTION_MASTER_wall_e:/usr/src/app/banrecords.json .
+docker cp PRODUCTION_MASTER_wall_e:/usr/src/app/commandstats.json .
+docker cp PRODUCTION_MASTER_wall_e:/usr/src/app/profilebucketsinprogress.json .
+docker cp PRODUCTION_MASTER_wall_e:/usr/src/app/userpoints.json .
+docker cp PRODUCTION_MASTER_wall_e:/usr/src/app/levels.json .
+docker cp PRODUCTION_MASTER_wall_e:/usr/src/app/reminders.json .
+
+docker exec PRODUCTION_MASTER_wall_e rm banrecords.json commandstats.json profilebucketsinprogress.json userpoints.json levels.json reminders.json
--- download_fixtures_and_media.sh
+++ download_fixtures_and_media.sh
@@ -6,38 +6,59 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${BASE_DIR}"
 
 ## Website media
+rm -fr /home/csss/staging_assets || true
+mkdir -p /home/csss/staging_assets/website/fixtures || true
+
 scp "create_fixture_jsons_csss_site.sh" csss@sfucsss.org:"/home/csss/create_jsons.sh"
 ssh csss@sfucsss.org "/home/csss/create_jsons.sh"
-scp -r csss@sfucsss.org:"/home/csss/csss-site/*.json" .
-ssh csss@sfucsss.org "rm -fr /home/csss/csss-site/*.json"
+scp csss@sfucsss.org:"/home/csss/csss-site/about.json" .
+scp csss@sfucsss.org:"/home/csss/csss-site/announcements.json" /home/csss/staging_assets/website/fixtures/.
+scp csss@sfucsss.org:"/home/csss/csss-site/auth.json" /home/csss/staging_assets/website/fixtures/.
+scp csss@sfucsss.org:"/home/csss/csss-site/csss_cron_info.json" /home/csss/staging_assets/website/fixtures/.
+scp csss@sfucsss.org:"/home/csss/csss-site/django_mailbox.json" /home/csss/staging_assets/website/fixtures/.
+scp csss@sfucsss.org:"/home/csss/csss-site/elections.json" /home/csss/staging_assets/website/fixtures/.
+scp csss@sfucsss.org:"/home/csss/csss-site/errors.json" /home/csss/staging_assets/website/fixtures/.
+scp csss@sfucsss.org:"/home/csss/csss-site/resource_management.json" /home/csss/staging_assets/website/fixtures/.
+
+ssh csss@sfucsss.org "rm /home/csss/csss-site/about.json"
+ssh csss@sfucsss.org "rm /home/csss/csss-site/announcements.json"
+ssh csss@sfucsss.org "rm /home/csss/csss-site/auth.json"
+ssh csss@sfucsss.org "rm /home/csss/csss-site/csss_cron_info.json"
+ssh csss@sfucsss.org "rm /home/csss/csss-site/django_mailbox.json"
+ssh csss@sfucsss.org "rm /home/csss/csss-site/elections.json"
+ssh csss@sfucsss.org "rm /home/csss/csss-site/errors.json"
+ssh csss@sfucsss.org "rm /home/csss/csss-site/resource_management.json"
+
 ssh csss@sfucsss.org "rm /home/csss/create_jsons.sh"
 
-rm -fr /home/csss/staging_assets/website/fixtures || true
-mkdir /home/csss/staging_assets/website/fixtures || true
 python3 ./redact_confidential_info.py
 mv about.json /home/csss/staging_assets/website/fixtures/.
-mv auth.json /home/csss/staging_assets/website/fixtures/.
-mv announcements.json /home/csss/staging_assets/website/fixtures/.
-mv django_mailbox.json /home/csss/staging_assets/website/fixtures/.
-mv elections.json /home/csss/staging_assets/website/fixtures/.
-mv resource_management.json /home/csss/staging_assets/website/fixtures/.
-mv csss_cron_info.json /home/csss/staging_assets/website/fixtures/.
-mv errors.json /home/csss/staging_assets/website/fixtures/.
 
-rm -fr /home/csss/staging_assets/website/mailbox_attachments || true
+mkdir -p /home/csss/staging_assets/website/mailbox_attachments || true
 scp -r csss@sfucsss.org:"/home/csss/media_root/mailbox_attachments" /home/csss/staging_assets/website/.
 
-rm -fr /home/csss/staging_assets/website/exec-photos
+mkdir -p /home/csss/staging_assets/website/exec-photos
 scp -r csss@sfucsss.org:"/home/csss/static_root/about_static/exec-photos" /home/csss/staging_assets/website/.
 rm -fr /home/csss/staging_assets/website/exec-photos/.git
 
 
 ## Wall_e media
+mkdir -p /home/csss/staging_assets/wall_e/fixtures || true
 
 scp "create_fixture_jsons_wall_e.sh" csss@jenkins.sfucsss.org:"/home/csss/create_jsons.sh"
+
 ssh csss@jenkins.sfucsss.org "/home/csss/create_jsons.sh"
-scp -r csss@jenkins.sfucsss.org:"/home/csss/wall_e.json" .
-ssh csss@jenkins.sfucsss.org "rm -fr /home/csss/wall_e.json"
+scp csss@jenkins.sfucsss.org:"/home/csss/banrecords.json" /home/csss/staging_assets/wall_e/fixtures/.
+scp csss@jenkins.sfucsss.org:"/home/csss/commandstats.json" /home/csss/staging_assets/wall_e/fixtures/.
+scp csss@jenkins.sfucsss.org:"/home/csss/profilebucketsinprogress.json" /home/csss/staging_assets/wall_e/fixtures/.
+scp csss@jenkins.sfucsss.org:"/home/csss/userpoints.json" /home/csss/staging_assets/wall_e/fixtures/.
+scp csss@jenkins.sfucsss.org:"/home/csss/levels.json" /home/csss/staging_assets/wall_e/fixtures/.
+scp csss@jenkins.sfucsss.org:"/home/csss/reminders.json" /home/csss/staging_assets/wall_e/fixtures/.
+
 ssh csss@jenkins.sfucsss.org "rm /home/csss/create_jsons.sh"
-mkdir -p /home/csss/staging_assets/wall_e/fixtures || true
-mv wall_e.json /home/csss/staging_assets/wall_e/fixtures/.
+ssh csss@jenkins.sfucsss.org "rm /home/csss/banrecords.json"
+ssh csss@jenkins.sfucsss.org "rm /home/csss/commandstats.json"
+ssh csss@jenkins.sfucsss.org "rm /home/csss/profilebucketsinprogress.json"
+ssh csss@jenkins.sfucsss.org "rm /home/csss/userpoints.json"
+ssh csss@jenkins.sfucsss.org "rm /home/csss/levels.json"
+ssh csss@jenkins.sfucsss.org "rm /home/csss/reminders.json"
